c3caaf27d6b7ff3820f9492208fc50efbe879e1e
thpossidente <tposside@gmail.com>
2017-02-19 17:13:54 -0500

    Normalization works

--- Spatial Pooling.R
+++ Spatial Pooling.R
@@ -8,21 +8,20 @@ alphabet.data <- read.table("C:/Users/research/Documents/GitHub/Int-Seg-Model/In
 #install.packages("splitstackshape")
 library("splitstackshape")
 alphabet.data <- cSplit(alphabet.data, "V1", ",", stripWhite = FALSE)
-names(alphabet.data) <- c('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17')
 alphabet.data.n <- alphabet.data[,-1]
 alphabet.data.n <- data.matrix(alphabet.data.n)
 
-normalization.by.column <- function(alphabet.data.n){  #doesn't work because 0 is minimum value and can't divide by zero
+normalization.by.column <- function(alphabet.data.n){ # normalizes values (by column) to values between 0 and 1
   alphabet.data.n.1 <- alphabet.data.n
   for(j in 1:ncol(alphabet.data.n)){
     pb <- txtProgressBar(min=0, max=nrow(alphabet.data.n), style=3)
     for(i in 1:nrow(alphabet.data.n)){
-      alphabet.data.n[i,j] <<- (alphabet.data.n.1[i,j] - min(alphabet.data.n.1[,j])) / (max(alphabet.data.n.1[,j] / min(alphabet.data.n.1[,j])))
+      alphabet.data.n[i,j] <<- (alphabet.data.n.1[i,j] - min(alphabet.data.n.1[,j])) / (max(alphabet.data.n.1[,j] - min(alphabet.data.n.1[,j])))
     }
       setTxtProgressBar(pb, i)
   }
 }
-
+normalization.by.column(alphabet.data.n)
 
 weights <- matrix(runif(n.inputs*n.outputs, min=0, max=1), nrow=n.inputs, ncol=n.outputs) #initialiize weights at random values between 1 and 0
 
@@ -30,7 +29,6 @@ sigmoid.activation <- function(x){
   return(1 / (1+exp(-x)))
 }
 
-?scale()
 forward.pass <- function(input){ #calculate output activations with "winner-takes-all" method
   outputs <- numeric(n.outputs)
   for(i in 1:n.outputs){
@@ -64,6 +62,4 @@ batch <- function(){
 
 batch()  #run training batches
 
-input <- alphabet.data.n[1,]
-test.output <- forward.pass(test.input)
-test.change.weight <- learning.rate * outputs[1] * input[1]
+
