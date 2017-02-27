58c45e96011ce073c80fcca1d5b97b11f9302c3c
thpossidente <tposside@gmail.com>
2017-02-27 13:41:38 -0500

    words list created, sigmoid act. problem

--- Spatial Pooling.R
+++ Spatial Pooling.R
@@ -1,4 +1,3 @@
-
 n.input <- 1600
 n.hidden <- 26
 n.output <- 50
@@ -44,6 +43,37 @@ alphabet <- list(
   y <- adrop(readPNG('C:/Users/research/Documents/GitHub/Int-Seg-Model/AlphabetPNG/Y.png')[,,1,drop=F], drop=3),
   z <- adrop(readPNG('C:/Users/research/Documents/GitHub/Int-Seg-Model/AlphabetPNG/Z.png')[,,1,drop=F], drop=3))
 
+
+words <- list(
+  cat <- cbind(c,a,t), bow <- cbind(b,o,w),
+  sip <- cbind(s,i,p), rad <- cbind(r,a,d),
+  zen <- cbind(z,e,n), two <- cbind(t,w,o),
+  rub <- cbind(r,u,b), vex <- cbind(v,e,x),
+  fox <- cbind(f,o,x), wry <- cbind(w,r,y),
+  vow <- cbind(v,o,w), zag <- cbind(z,a,g),
+  quo <- cbind(q,u,o), fry <- cbind(f,r,y),
+  the <- cbind(t,h,e), pew <- cbind(p,e,w),
+  dug <- cbind(d,u,g), keg <- cbind(k,e,w),
+  yak <- cbind(y,a,k), tax <- cbind(t,a,x),
+  jaw <- cbind(j,a,w), who <- cbind(w,h,o),
+  lax <- cbind(l,a,x), til <- cbind(t,i,l),
+  sin <- cbind(s,i,n), mud <- cbind(m,u,d),
+  yap <- cbind(y,a,p), orb <- cbind(o,r,b),
+  ply <- cbind(p,l,y), cry <- cbind(c,r,y),
+  tom <- cbind(t,o,m), coy <- cbind(c,o,y),
+  any <- cbind(a,n,y), jot <- cbind(j,o,t),
+  she <- cbind(s,h,e), gig <- cbind(g,i,g),
+  axe <- cbind(a,x,e), icy <- cbind(i,c,y),
+  elm <- cbind(e,l,m), owl <- cbind(o,w,l),
+  gag <- cbind(g,a,g), nun <- cbind(n,u,n),
+  jay <- cbind(j,a,y), rye <- cbind(r,y,e),
+  apt <- cbind(a,p,t), sty <- cbind(s,t,y),
+  lit <- cbind(l,i,t), why <- cbind(w,h,y),
+  hue <- cbind(h,u,e), use <- cbind(u,s,e)
+)
+
+
+
 input.hidden.weights <- matrix(runif(n.input*n.hidden, min=0, max=1), nrow=n.input, ncol=n.hidden) #initialiize weights at random values between 1 and 0
 hidden.output.weights <- matrix(runif(n.hidden*n.output, min=0, max=1), nrow=n.hidden, ncol=n.output)
 
@@ -62,26 +92,45 @@ forward.pass <- function(input){ #calculate output activations with "winner-take
   hidden[which.max(hidden)] <- 1
   hidden[hidden != max(hidden)] <- 0
   return(hidden)
+  #output <- numeric(n.output)
+  #for(b in 1:n.output){
+  #  output[b] <- sigmoid.activation(sum(hidden * hidden.output.weights[,b]))
+  #}
+  #output[which.max(output)] <- 1
+  #output[output != max(output)] <- 0
+  #return(list(hidden=hidden, output=output))
 }
 
-
-trace.update <- function(input, input.hidden.weights, hidden.output.weights, trace.hidden, trace.output){ 
+trace.update <- function(input, input.hidden.weights, trace.hidden){
+  #trace.update <- function(input, input.hidden.weights, hidden.output.weights, trace.hidden, trace.output){ 
   
   hidden <- forward.pass(input)
+  #forward.pass.results <- forward.pass(input)
+  #hidden <- forward.pass.results$hidden
+  #output <- forward.pass.results$output
+  
   for(i in 1:n.hidden){
     trace.hidden[i] <- (1 - trace.param.hidden) * trace.hidden[i]  + trace.param.hidden * hidden[i] 
     input.hidden.weights[,i] <- input.hidden.weights[,i] + learning.rate * trace.hidden[i] * (input - input.hidden.weights[,i])  
   }
-  return(list(trace.hidden=trace.hidden, trace.ouput=trace.output, input.hidden.weights=input.hidden.weights, hidden.output.weights=hidden.output.weights))
+  return(list(trace.hidden=trace.hidden, input.hidden.weights=input.hidden.weights))
+  
+  #for(b in 1:n.output){
+  #  trace.output <- (1 - trace.param.output) * trace.output[b] + trace.param.output * output[b]
+  #  hidden.output.weights[,b] <- hidden.output.weights[,b] + learning.rate * trace.output[i] * (hidden - hidden.output.weights[,b])
+  #}
+  #return(list(trace.hidden=trace.hidden, trace.ouput=trace.output, input.hidden.weights=input.hidden.weights, hidden.output.weights=hidden.output.weights))
 }
 
 
 batch <- function(n.epochs){ 
+  
   for(i in 1:n.epochs){
     letter <- alphabet[[sample(1:26,1, replace = T)]]
-    results <- trace.update(letter, input.hidden.weights, hidden.output.weights, trace.hidden, trace.output)
+    #results <- trace.update(letter, input.hidden.weights, hidden.output.weights, trace.hidden, trace.output)
+    results <- trace.update(letter, input.hidden.weights, trace.hidden)
     input.hidden.weights <- results$input.hidden.weights
-    hidden.output.weights <- results$hidden.output.weights
+    #hidden.output.weights <- results$hidden.output.weights
   }
   return(output.storage())
 }
@@ -95,12 +144,12 @@ test <- output.storage()
 ## entropy testing functions ##
 
 output.storage <- function(){ #stores outputs 
-  outputs <- matrix(0, nrow = n.test, ncol = n.hidden)
+  hidden.outputs <- matrix(0, nrow = n.test, ncol = n.hidden)
   for(i in 1:26){
-    one.output <- forward.pass(alphabet[[i]])
-    outputs[i,] <- one.output
+    one.hidden <- forward.pass(alphabet[[i]])
+    hidden.outputs[i,] <- one.hidden
   }
-  return(outputs)
+  return(hidden.outputs)
 }
 
 
