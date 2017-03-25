162530350401b907dbebc393887469302ed57052
thpossidente <tposside@gmail.com>
2017-03-25 18:33:04 -0400

    Debugging 3rd layer implementation

--- Spatial Pooling.R
+++ Spatial Pooling.R
@@ -1,5 +1,5 @@
 n.input <- 1600
-n.hidden <- 52
+n.hidden <- 26
 n.output <- 50
 learning.rate <- 0.05
 n.epochs <- 5000
@@ -10,7 +10,8 @@ trace.param.hidden <- 1 # value of 1 indicates pure hebbian learning. Closer to 
 trace.param.output <- 0.2
 hidden.bias.param.minus <- 1
 hidden.bias.param.plus <- 0.05
-
+output.bias.param.minus <- 1
+output.bias.param.plus <- 0.05
 
 #install.packages('png')
 library('png')
@@ -79,9 +80,6 @@ sigmoid.activation <- function(x){
   return(x)
 }
 
-#activation.redistribution <- function(x){
-#  return(x^5)
-#}
 
 forward.pass <- function(input, input.hidden.weights, hidden.bias.weights){ #calculate output activations with "winner-takes-all" method
   
@@ -92,19 +90,28 @@ forward.pass <- function(input, input.hidden.weights, hidden.bias.weights){ #cal
   hidden[hidden != max(hidden)] <- 0
   hidden[which.max(hidden)] <- 1
   return(hidden)
+}
 
-  #output <- numeric(n.output)
-  #for(b in 1:n.output){
-  #  output[b] <- sigmoid.activation(sum(hidden * hidden.output.weights[,b] + (1 + output.bias[b,1])))
-  #}
-  #output[which.max(output)] <- 1
-  #output[output != max(output)] <- 0
-  #return(list(hidden=hidden, output=output))
+forward.pass.2 <- function(input, input.hidden.weights, hidden.bias.weights, hidden.output.weights, output.bias.weights){ #calculate output activations with "winner-takes-all" method
+  
+  hidden <- numeric(n.hidden)
+  for(i in 1:n.hidden){
+    hidden[i] <- sigmoid.activation(sum(input * input.hidden.weights[,i]) + hidden.bias.weights[i,1])
+  }
+  hidden[hidden != max(hidden)] <- 0
+  hidden[which.max(hidden)] <- 1
+  
+  output <- numeric(n.output)
+  for(b in 1:n.output){
+    output[b] <- sigmoid.activation(sum(hidden * hidden.output.weights[,b] +  output.bias.weights[b,1]))
+  }
+  output[output != max(output)] <- 0
+  output[which.max(output)] <- 1
+  return(list(hidden=hidden, output=output))
 }
 
 
 trace.update <- function(input, input.hidden.weights, trace.hidden, hidden.bias.weights){
-#trace.update <- function(input, input.hidden.weights, hidden.output.weights, trace.hidden, trace.output){ 
   
   hidden <- forward.pass(input, input.hidden.weights, hidden.bias.weights)
   
@@ -120,26 +127,89 @@ trace.update <- function(input, input.hidden.weights, trace.hidden, hidden.bias.
     }
   }
   
-  #forward.pass.results <- forward.pass(input)
-  #hidden <- forward.pass.results$hidden
-  #output <- forward.pass.results$output
-  
   for(i in 1:n.hidden){
     trace.hidden[i] <- (1 - trace.param.hidden) * trace.hidden[i] + trace.param.hidden * hidden[i] 
     input.hidden.weights[,i] <- input.hidden.weights[,i] + learning.rate * trace.hidden[i] * (input - input.hidden.weights[,i])
   }
+  
   return(list(
     trace.hidden = trace.hidden,
     hidden = hidden,
     input.hidden.weights = input.hidden.weights, 
     hidden.bias.weights = hidden.bias.weights
   ))
+}
+
+learning.measure <- function(input.hidden.weights){
+  all.letters.compared <- numeric(26)
+  best.fit <- numeric(n.hidden)
+  for(i in 1:n.hidden){
+    for(h in 1:26){
+      all.letters.compared[h] <- sum(abs(input.hidden.weights[,i] - alphabet[[h]]))
+    }
+    best.fit[i] <- min(all.letters.compared)
+  }
+  return(best.fit)
+}
+
+display.learning.curves <- function(results){
+  for(i in 1:n.hidden){
+    layout(matrix(1:4, nrow=2))
+    plot(results$learning.curve[,i], main=paste("Node",i))
+    plot(results$bias.tracker[,i])
+    image(matrix(results$input.hidden.weights[,i], nrow = 40))
+  }
+}
+
+trace.update.2 <- function(input, input.hidden.weights, trace.hidden, hidden.bias.weights, hidden.output.weights, trace.output, output.bias.weights){
   
-  #for(b in 1:n.output){
-  #  trace.output <- (1 - trace.param.output) * trace.output[b] + trace.param.output * output[b]
-  #  hidden.output.weights[,b] <- hidden.output.weights[,b] + learning.rate * trace.output[i] * (hidden - hidden.output.weights[,b])
-  #}
-  #return(list(trace.hidden=trace.hidden, trace.ouput=trace.output, input.hidden.weights=input.hidden.weights, hidden.output.weights=hidden.output.weights, hidden.bias.weights=hidden.bias.weights, output.bias.weights=output.bias.weights))
+  forward.pass.results <- forward.pass.2(input, input.hidden.weights, hidden.bias.weights, hidden.output.weights, output.bias.weights)
+  hidden <- forward.pass.results$hidden
+  output <- forward.pass.results$output
+  
+  for(h in 1:n.hidden){
+    if(hidden[h] == 1){
+      hidden.bias.weights[h,1] <- hidden.bias.weights[h,1] - hidden.bias.param.minus
+    }
+    if(hidden[h] == 0){
+      hidden.bias.weights[h,1] <- hidden.bias.weights[h,1] + hidden.bias.param.plus
+    }
+    if(hidden.bias.weights[h,1] < 0){
+      hidden.bias.weights[h,1] <- 0
+    }
+  }
+
+  for(i in 1:n.hidden){
+    trace.hidden[i] <- (1 - trace.param.hidden) * trace.hidden[i] + trace.param.hidden * hidden[i] 
+    input.hidden.weights[,i] <- input.hidden.weights[,i] + learning.rate * trace.hidden[i] * (input - input.hidden.weights[,i])
+  }
+  
+  for(j in 1:n.output){
+    if(output[j] == 1){
+      output.bias.weights[j,1] <- output.bias.weights[j,1] - output.bias.param.minus
+    }
+    if(output[j] == 0){
+      output.bias.weights[j,1] <- output.bias.weights[j,1] + output.bias.param.plus
+    }
+    if(output.bias.weights[j,1] < 0){
+      output.bias.weights[j,1] <- 0
+    }
+  }
+  
+  for(b in 1:n.output){
+    trace.output[b] <- (1 - trace.param.output) * trace.output[b] + trace.param.output * output[b]
+    hidden.output.weights[,b] <- hidden.output.weights[,b] + learning.rate * trace.output[b] * (hidden - hidden.output.weights[,b])
+  }
+  
+  return(list(
+    trace.hidden=trace.hidden, 
+    hidden = hidden,
+    input.hidden.weights=input.hidden.weights, 
+    hidden.bias.weights=hidden.bias.weights,
+    trace.ouput=trace.output,
+    output=output,
+    hidden.output.weights=hidden.output.weights,
+    output.bias.weights=output.bias.weights))
 }
 
 
@@ -168,26 +238,22 @@ display.learning.curves <- function(results){
 
 batch <- function(n.epochs){
   
-  # network properties
+  # network properties #
   input.hidden.weights <- matrix(runif(n.input*n.hidden, min=0, max=0.05), nrow=n.input, ncol=n.hidden) #initialiize weights at random values between 0 and 0.05
   hidden.bias.weights <- matrix(0, nrow=n.hidden, ncol=1)
   
-  #hidden.output.weights <- matrix(runif(n.hidden*n.output, min=0, max=0.05), nrow=n.hidden, ncol=n.output)
-  #output.bias.weights <- matrix(0, nrow=n.output, ncol=1)
-  
-  # tracking learning
+  # tracking learning #
   learning.curve <- matrix(0, nrow = n.epochs/100, ncol = n.hidden) #initializes learning data matrix
   bias.tracker <- matrix(0, nrow = n.epochs/100, ncol = n.hidden) #initializes learning data matrix
   hidden.win.tracker <- matrix(0, nrow=n.epochs, ncol= n.hidden)
+  
   pb <- txtProgressBar(min=1, max=n.epochs,style=3)
   for(i in 1:n.epochs){
     letter <- alphabet[[sample(1:26,1, replace = T)]]
-    #results <- trace.update(letter, input.hidden.weights, hidden.output.weights, trace.hidden, trace.output)
     results <- trace.update(letter, input.hidden.weights, trace.hidden, hidden.bias.weights)
     input.hidden.weights <- results$input.hidden.weights
     trace.hidden <- results$trace.hidden
     hidden.bias.weights <- results$hidden.bias.weights
-    #hidden.output.weights <- results$hidden.output.weights
     hidden.win.tracker[i,] <- results$hidden
     if(i %% 100 == 0){
       learning.curve[i / 100,] <- learning.measure(input.hidden.weights)
@@ -205,27 +271,47 @@ batch <- function(n.epochs){
 }
 
 
-#batch <- function(n.epochs){ 
+batch.2 <- function(n.epochs){ 
+  # network properties #
+  input.hidden.weights <- matrix(runif(n.input*n.hidden, min=0, max=0.05), nrow=n.input, ncol=n.hidden) #initialiize weights at random values between 0 and 0.05
+  hidden.bias.weights <- matrix(0, nrow=n.hidden, ncol=1)
+  hidden.output.weights <- matrix(runif(n.hidden*n.output, min=0, max=0.05), nrow=n.hidden, ncol=n.output)
+  output.bias.weights <- matrix(0, nrow=n.output, ncol=1)
   
-  #for(i in 1:n.epochs){
-    #word <- words[[sample(1:50,1, replace = T)]]
-    #for(b in 1:(length(word)/n.input)){
-      #letter[b] <- word[,b]
-      #results <- trace.update(letter[b], input.hidden.weights, hidden.output.weights, trace.hidden, trace.output)
-      #results <- trace.update(letter[b], input.hidden.weights, trace.hidden)
-      #input.hidden.weights <- results$input.hidden.weights
-      #hidden.output.weights <- results$hidden.output.weights
-    #}
-  #}
-  #return(output.storage())
-#}
+  # tracking learning #
+  learning.curve <- matrix(0, nrow = n.epochs/100, ncol = n.hidden) #initializes learning data matrix
+  bias.tracker <- matrix(0, nrow = n.epochs/100, ncol = n.hidden) #initializes learning data matrix
+  hidden.win.tracker <- matrix(0, nrow=n.epochs, ncol= n.hidden)
+  
+  pb <- txtProgressBar(min=1, max=n.epochs,style=3)
+  for(i in 1:n.epochs){
+    word <- words[[sample(1:50,1, replace = T)]]
+    for(b in 1:(length(word)/n.input)){
+      letter <- word[,b]
+      results <- trace.update.2(letter, input.hidden.weights, trace.hidden, hidden.bias.weights, hidden.output.weights, trace.output, output.bias.weights)
+      input.hidden.weights <- results$input.hidden.weights
+      hidden.output.weights <- results$hidden.output.weights
+      trace.hidden <- results$trace.hidden
+      hidden.bias.weights <- results$hidden.bias.weights
+      hidden.output.weights <- results$hidden.output.weights
+      trace.output <- results$trace.output
+      output.bias.weights <- results$output.bias.weights
+      setTxtProgressBar(pb, i)
+    }
+  }
+  return(list(
+    input.hidden.weights=input.hidden.weights,
+    hidden.output.weights=hidden.output.weights
+  ))
+}
 
-results <- batch(n.epochs) #run training batches
-#input.hidden.weights <- results$input.hidden.weights#save weights to global envirn.
-#hidden.bias.weights <- results$hidden.bias.weights
-#learning.curve <- results$learning.curve #save learning data to global envirn.
-#weight.images() #connection weight visualization by row (26). Letters will appear rotated 180 degrees.
+
+
+
+results <- batch.2(n.epochs) #run training batches
 display.learning.curves(results) #visualize learning by plotting weight similarity to alphabet input every 100 epochs
+
+
 
 
 ## output storage func. and weight image generation ##
@@ -247,4 +333,3 @@ weight.images <- function(){
 }
 
 image(results$hidden.win.tracker)
-
