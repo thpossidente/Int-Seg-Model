d70f10282289f22b833eedaf2e77917c58c56a61
thpossidente <tposside@gmail.com>
2017-04-12 14:02:35 -0400

    10%actOutputParamTweaking

--- Spatial Pooling.R
+++ Spatial Pooling.R
@@ -1,15 +1,15 @@
 library(ggplot2)
 
 n.input <- 1600
-n.hidden <- 150
-n.output <- 9
-learning.rate.hidden <- 0.00005
+n.hidden <- 100
+n.output <- 20
+learning.rate.hidden <- 0.1
 learning.rate.output <- 0.2
-n.epochs <- 50000
+n.epochs <- 5000
 trace.param.hidden <- 1 # value of 1 indicates pure hebbian learning. Closer to zero, more of 'history' of node activation is taken into account
 trace.param.output <- 0.6
 hidden.bias.param.minus <- 2
-hidden.bias.param.plus <- 0.05
+hidden.bias.param.plus <- 0.1
 output.bias.param.minus <- 0
 output.bias.param.plus <- 0
 
