16ddb307613b2dd98a75b13e1e5a2d1d70495dc6
Thomas Possidente <thpossidente@vassar.edu>
2018-01-27 13:13:17 -0500

    working on cpp

--- Spatial Pooling.R
+++ Spatial Pooling.R
@@ -8,7 +8,7 @@ learning.rate.hidden <- 0.005
 learning.rate.output <- 0.005
 n.epochs <- 10000
 trace.param.hidden <- 1 # value of 1 indicates pure hebbian learning. Closer to zero, more of 'history' of node activation is taken into account
-trace.param.output <- 0.75 #0.75
+trace.param.output <- 0.1 #0.75
 hidden.bias.param.minus <- 2
 hidden.bias.param.plus <- 0.0005
 output.bias.param.minus <- 1 #0
@@ -34,10 +34,9 @@ results <- batch(n.epochs) #run training batches
 visualize.hidden.layer.learning(results$history)
 display.learning.curves(results) 
 display.output.bias.tracker(results)
-visualize.letter.activations(results$network, l)
+visualize.letter.activations(results$network, s)
 visualize.output.act.match()
 temp.layer.many.activations(network, words)
 
 network <- results$network
-
 
--- multi-layer-network-cpp.cpp
+++ multi-layer-network-cpp.cpp
@@ -17,11 +17,16 @@ NumericVector timesTwo(NumericVector x) {
   return x * 2;
 }
 
+
+
 List forwardPass(NumericVector input, NumericMatrix inputToHiddenWeights, NumericVector hiddenBiasWeights, NumericMatrix hiddenToOutputWeights, NumericVector outputBiasWeights){
   
+  Environment env = Environment::global_env();
+  
   NumericVector hidden;
-  for(int i=0; i<hidden.length(); i++){
-    hidden[i] += hiddenBiasWeights[i,1]
+  int n_hidden = env["n.hidden"];
+  for(int i=0; i<n_hidden; i++){
+    hidden[i] += hiddenBiasWeights[i,1];
     for(int j=0; j<input.length(); j++){
       if(inputToHiddenWeights[j,i] != NAN) {
         hidden[i] += input[j] * inputToHiddenWeights[j,i];
@@ -29,9 +34,50 @@ List forwardPass(NumericVector input, NumericMatrix inputToHiddenWeights, Numeri
     }
   }
   
-  for(int, c=0; i<){
-    
+  int largest;
+  int percentActInput = env["percent.act.input"];
+  int number = ceil(percentActInput * n_hidden);
+  for(int c=0; c<number; c++){
+    largest = max_element(hidden.begin(), hidden.end());
+    hidden[largest] = -1;
   }
+  
+  for(int x=0; x<n_hidden; x++){
+    if(hidden[x] == -1){
+      hidden[x] = 1;
+    } else{
+      hidden[x] = 0;
+    }
+  }
+  
+  NumericVector output;
+  int n_output = env["n.output"];
+  for(int z=0; z<n_output; z++){
+    output[z] += outputBiasWeights[z,1];
+    for(int h=0; h<n_hidden; h++){
+      if(hiddenToOutputWeights[h,z] != NAN) {
+        output[h] += hidden[h] * hiddenToOutputWeights[h,z];
+      }
+    }
+  }
+  
+  int largest1;
+  int percentActOutput = env["percent.act.output"];
+  int number1 = ceil(percentActOutput * n_output);
+  for(int k=0; k<number1; k++){
+    largest1 = max_element(output.begin(), output.end());
+    output[largest1] = -1;
+  }
+  
+  for(int d=0; d<n_output; d++){
+    if(output[d] == -1){
+      output[d] = 1;
+    } else{
+      output[d] = 0;
+    }
+  }
+  
+  return()
 }
 
 // You can include R code blocks in C++ files processed with sourceCpp
