60897aec93401c56f1513d4833b732226cf24c01
Thomas Possidente <thpossidente@vassar.edu>
2018-02-06 11:44:45 -0500

    Commit

--- Spatial.Pooling.R
+++ Spatial.Pooling.R
@@ -5,10 +5,6 @@ source('Load Letters.R')
 source('Visualize Output.R')
 source('multi-layer-network.R')
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
 n.input <- 1600
 n.hidden <- 100
 n.output <- 30
@@ -17,10 +13,6 @@ learning.rate.output <- 0.005
 n.epochs <- 10000
 trace.param.hidden <- 1 # value of 1 indicates pure hebbian learning. Closer to zero, more of 'history' of node activation is taken into account
 trace.param.output <- 0.75 #0.75
-<<<<<<< HEAD
-=======
-trace.param.output <- 0.25 #0.75
->>>>>>> origin/master
 hidden.bias.param.minus <- 2
 hidden.bias.param.plus <- 0.0005
 output.bias.param.minus <- 0 #0
@@ -35,11 +27,6 @@ input.gen.parameter <- 0 # if 1: temporal pattern of input for one system, rando
                          # if 0: Next inputs are predicted by combination of both systems' previous inputs - one system alone cannot predict next inputs
                          # if 0.5: inputs for each system consistently co-occur
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> origin/master
 ## RUN ##
 
 results <- batch(n.epochs) #run training batches
@@ -49,13 +36,10 @@ display.learning.curves(results)
 display.output.bias.tracker(results)
 visualize.letter.activations(results$network, s)
 visualize.output.act.match()
-<<<<<<< HEAD
 temp.layer.activations.many <- temp.layer.many.activations(network, words)
 output.trace.track <- results$history$output.trace.tracker
-=======
 n <- temp.layer.many.activations(network, words)
 output.trace.tracker.results <- results$history$trace.output.tracker
->>>>>>> origin/master
 
 network <- results$network
 
