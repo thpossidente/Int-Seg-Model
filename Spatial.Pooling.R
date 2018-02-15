2183a8b470323f867991522b04cf314a866a5b08
Thomas Possidente <thpossidente@vassar.edu>
2018-02-15 12:58:28 -0500

    commit

--- Spatial.Pooling.R
+++ Spatial.Pooling.R
@@ -10,6 +10,7 @@ n.hidden <- 100
 n.output <- 30
 learning.rate.hidden <- 0.005
 learning.rate.output <- 0.005
+n.epochs <- 10
 trace.param.hidden <- 1 # value of 1 indicates pure hebbian learning. Closer to zero, more of 'history' of node activation is taken into account
 trace.param.output <- 0.25 #0.75
 trace.param.output <- 0.75 #0.75
