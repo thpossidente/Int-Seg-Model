6c7ddc5c25657bccd4e8de1df81848460750a6ab
thpossidente <thpossidente@vassar.edu>
2018-03-02 00:14:44 -0500

    Commit

--- forwardPassCpp.cpp
+++ forwardPassCpp.cpp
@@ -2,18 +2,120 @@
 #include <RcppArmadilloExtensions/sample.h>
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
+
+#include <RcppCommon.h>
+
+// Flags for C++ compiler: include Boost headers, use the C++11 standard
+
+// [[Rcpp::depends(BH)]]
+// [[Rcpp::plugins("cpp11")]]
+
+// Third party library includes that provide the template class of ublas
+#include <boost/numeric/ublas/matrix_sparse.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+
+// Provide Forward Declarations
+namespace Rcpp {
+
+namespace traits{
+
+// Setup non-intrusive extension via template specialization for
+// 'ublas' class boost::numeric::ublas
+
+// Support for wrap
+template <typename T> SEXP wrap(const boost::numeric::ublas::vector<T> & obj);
+
+// Support for as<T>
+template <typename T> class Exporter< boost::numeric::ublas::vector<T> >;
+
+}
+}
+
+// -------------- Stage 2: Including Rcpp.h
+
+// ------ Place <Rcpp.h> AFTER the Forward Declaration!!!!
+
+#include <Rcpp.h>
+
+// ------ Place Implementations of Forward Declarations AFTER <Rcpp.h>!
+
+// -------------- Stage 3: Implementation the Declarations
+
+// Define template specializations for as<> and wrap
+namespace Rcpp {
+
+namespace traits{
+
+// Defined wrap case
+template <typename T> SEXP wrap(const boost::numeric::ublas::vector<T> & obj){
+  const int RTYPE = Rcpp::traits::r_sexptype_traits<T>::rtype ;
+  
+  return Rcpp::Vector< RTYPE >(obj.begin(), obj.end());
+};
+
+
+// Defined as< > case
+template<typename T> class Exporter< boost::numeric::ublas::vector<T> > {
+  typedef typename boost::numeric::ublas::vector<T> OUT ;
+  
+  // Convert the type to a valid rtype. 
+  const static int RTYPE = Rcpp::traits::r_sexptype_traits< T >::rtype ;
+  Rcpp::Vector<RTYPE> vec;
+  
+public:
+  Exporter(SEXP x) : vec(x) {
+    if (TYPEOF(x) != RTYPE)
+      throw std::invalid_argument("Wrong R type for mapped 1D array");
+  }
+  OUT get() {
+    
+    // Need to figure out a way to perhaps do a pointer pass?
+    OUT x(vec.size());
+    
+    std::copy(vec.begin(), vec.end(), x.begin()); // have to copy data
+    
+    return x;
+  }
+};
+}
+}
+
+// -------------- Stage 4: Testing
+
+// Here we define a shortcut to the Boost ublas class to enable multiple ublas
+// types via a template.
+// ublas::vector<T> => ublas::vector<double>, ... , ublas::vector<int>
+namespace ublas = ::boost::numeric::ublas;
+
+
+// [[Rcpp::export]]
+void containment_test(Rcpp::NumericVector x1) {
+  
+  Rcpp::Rcout << "Converting from Rcpp::NumericVector to ublas::vector<double>" << std::endl;
+  
+  // initialize the vector to all zero
+  ublas::vector<double> x = Rcpp::as< ublas::vector<double> >(x1); 
+  
+  Rcpp::Rcout << "Running output test with ublas::vector<double>" << std::endl;
+  
+  for (unsigned i = 0; i < x.size (); ++ i)
+    Rcpp::Rcout  << x(i) << std::endl;
+  
+  Rcpp::Rcout << "Converting from ublas::vector<double> to Rcpp::NumericVector" << std::endl;
+  
+  Rcpp::NumericVector test = Rcpp::wrap(x);
+  
+  Rcpp::Rcout << "Running output test with Rcpp::NumericVector" << std::endl;
+  
+  for (unsigned i = 0; i < test.size (); ++ i)
+    Rcpp::Rcout  << test(i) << std::endl;
+  
+}
 
 
 
+
+// [[Rcpp::export]]
 NumericVector noiseInLetter(NumericVector input, int n_input, float letterNoiseParam, int n_epochs){
   
   NumericVector vect(n_epochs);
@@ -56,8 +158,8 @@ DataFrame callFunction(List letters, List network, Function batchHiddenLayerLear
 
 // [[Rcpp::export]]
 
-SEXP callFunction1(List network, List words, Function testWordContinuity){
-  SEXP res = testWordContinuity(network, words);
+ublas::vector<double> callFunction1(List network, List words, Function testWordContinuity){
+  ublas::vector<double> res = testWordContinuity(network, words);
   return res;
 }
 
