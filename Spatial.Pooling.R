bcac8d363f32e16dd42e11cced28c8c7a39c5470
Thomas Possidente <thpossidente@vassar.edu>
2018-05-03 12:22:09 -0400

    Commit

--- Spatial.Pooling.R
+++ Spatial.Pooling.R
@@ -7,7 +7,7 @@ source('multi-layer-network.R')
 
 
 n.input <- 1600
-n.hidden <- 150
+n.hidden <- 500
 n.output <- 10  #Must be multiple of 10 due to activation percentage calculation
 learning.rate.hidden <- 0.005
 learning.rate.output <- 0.009 # 0.009
@@ -16,13 +16,8 @@ trace.param.hidden <- 1 # value of 1 indicates pure hebbian learning. Closer to 
 trace.param.output <- 0.7 #0.86
 hidden.bias.param.minus <- 1
 hidden.bias.param.plus <- 0.0005
-<<<<<<< HEAD
-output.bias.param.minus <- 0.5 #0
-output.bias.param.plus <- 0.0005 #0 
-=======
-output.bias.param.minus <- 0.005 #0
-output.bias.param.plus <- 0.00005 #0 
->>>>>>> dc1f800ded5dcd6ec915afd807ebb701805960e2
+output.bias.param.minus <- 0.25 #0
+output.bias.param.plus <- 0.0001 #0
 sparseness.percent <- 0.75  # sparseness.percent is % nodes inactive #0.75
 num.inputs.generated <- n.input/2 # half of total inputs
 integration.parameter <- 1 #0 is totally segregated, 1 is totally integrated
@@ -42,18 +37,16 @@ visualize.hidden.layer.learning(results$history)
 visualize.output.act.match()
 plot(x=seq(from = 1, to = n.epochs/100, by = 1), y=results$history$output.act.unique.tracker, type='b', ylim=c(0,1))
 test.word.continuity1(results$network, words)
-
-display.learning.curves(results) 
-visualize.letter.activations(results$network, j)
-temp.layer.activations.many <- temp.layer.many.activations(network, words)
-output.trace.tracker.results <- results$history$output.trace.tracker
+mutual.info.output()
+plot(x=seq(from=100, to=10000, by=100), y=results$history$output.bias.tracker[,6], type='b', ylim=c(0,0.02))
 
 
-plot(x=seq(from = 1, to = 100, by = 1), y=output.trace.tracker.results[,30], type = "b")
+# display.learning.curves(results) 
+# visualize.letter.activations(results$network, j)
+# temp.layer.activations.many <- temp.layer.many.activations(network, words)
+# output.trace.tracker.results <- results$history$output.trace.tracker
+# 
+# 
+# plot(x=seq(from = 1, to = 100, by = 1), y=output.trace.tracker.results[,30], type = "b")
 
 
-<<<<<<< HEAD
-plot(x=seq(from=100, to=15000, by=100), y=results$history$output.bias.tracker[,7], type='b', ylim=c(0,0.02))
-=======
-plot(x=seq(from=100, to=15000, by=100), y=results$history$output.bias.tracker[,10], type='b', ylim=c(0,0.005))
->>>>>>> dc1f800ded5dcd6ec915afd807ebb701805960e2
