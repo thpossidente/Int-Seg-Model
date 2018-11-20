71668b12f16a34529678626272fe0296b0eac326
Tom Possidente <tposside@gmail.com>
2018-11-20 11:48:36 -0500

    re-added notes section

--- MI.Calculation.Script.R
+++ MI.Calculation.Script.R
@@ -2,7 +2,7 @@ MI.calc <- function(im_size, rec_size0, noise, num){
   rec_size <- c(1,3,13,39)
   output <- vector();
   testing <- generate_MI_matrices(im_size, rec_size0, noise,num)
-  for(i in 1:3){
+  for(i in 1:4){
     output[i] = averageMIperPixelPair(testing, rec_size[i], rec_size[i])
   }
   
