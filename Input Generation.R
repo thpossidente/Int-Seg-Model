24faf499c4aee3d765ef9edefb446d756dab6a46
Tom Possidente <tposside@gmail.com>
2019-01-02 17:34:48 -0500

    importing/preprocessing inputs from R to CSV to python somewhat functional

--- Input Generation.R
+++ Input Generation.R
@@ -5,7 +5,7 @@ generate_input1 <- function(size, rec_field, noise, schemes){ # generates inputs
                                                      # is 2 and must be even. Size must be even. Noise is percent of inputs
                                                      # that are flipped from 0 to 1 or 1 to 0.
   inp_mat <- matrix(0, size, size)
-  nums <- sample(1:4,((size^2)/(rec_field^2)),replace = FALSE)
+  nums <- sample(1:4,((size^2)/(rec_field^2)),replace = TRUE)
   counter <- 0
   
   for(i in 1:(size/rec_field)){
